1680d242f0235e6541f3d55ef814adce83c8a5b6
truncntt <nguyenducthao.cntt@gmail.com>
2024-12-05 12:19:52 +0700

    Fix bug Nextjs

--- src/hooks/use-outside-click.ts
+++ src/hooks/use-outside-click.ts
@@ -2,14 +2,14 @@ import React, { useEffect } from "react";
 
 export const useOutsideClick = (
   ref: React.RefObject<HTMLDivElement>,
-  callback: Function
+  callback: (event: MouseEvent | TouchEvent) => void
 ) => {
   useEffect(() => {
-    const listener = (event: any) => {
-      if (!ref.current || ref.current.contains(event.target)) {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref.current || ref.current.contains(event.target as Node)) {
         return;
       }
-      callback(event);
+      callback(event); // Gọi hàm callback với kiểu sự kiện
     };
 
     document.addEventListener("mousedown", listener);
